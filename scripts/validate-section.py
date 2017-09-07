660d17d9c112e1bf9aeee90119464b55f436c44f
E Rasche <hxr@hx42.org>
2017-09-07 11:04:44 +0200

    lint section ids as well as labels

--- scripts/validate-section.py
+++ scripts/validate-section.py
@@ -18,7 +18,7 @@ for file in glob.glob("*.yaml"):
 
     for tool in w['tools']:
 
-        if 'tool_panel_section_label' not in tool:
+        if 'tool_panel_section_label' not in tool and 'tool_panel_section_id' not in tool:
             # Ignore data managers
             if 'data_manager' in tool['name']:
                 continue
@@ -27,8 +27,12 @@ for file in glob.glob("*.yaml"):
             log.error("[%s] Tool %s %s missing section", file, tool['owner'], tool['name'])
             continue
 
-        if tool['tool_panel_section_label'] not in section_list:
+        if 'tool_panel_section_label' in tool and tool['tool_panel_section_label'] not in section_list:
             exit_code = 1
             log.error("[%s] Tool %s %s unknown section (%s)", file, tool['owner'], tool['name'], tool['tool_panel_section_label'])
+
+        if 'tool_panel_section_id' in tool and tool['tool_panel_section_id'] not in section_list:
+            exit_code = 1
+            log.error("[%s] Tool %s %s unknown section (%s)", file, tool['owner'], tool['name'], tool['tool_panel_section_id'])
 
 sys.exit(exit_code)
